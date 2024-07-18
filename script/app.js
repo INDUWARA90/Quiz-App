3ac2efc31f0a5b6fd0ecaf4586efd117aa5a0f80
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-07-18 07:37:27 +0530

    add Question Selecting Function and Add Instruction Page

--- script/app.js
+++ script/app.js
@@ -1,31 +1,40 @@
-const quectionElemt=document.querySelector(".container-quection");
-const buttonElemnt=document.querySelector(".conatainer-buttons");
-const buttonNext=document.querySelector(".next");
+const HomePage=document.querySelector('.container');
+const quectionElemt = document.querySelector(".container-quection");
+const buttonElemnt = document.querySelector(".conatainer-buttons");
+const buttonNext = document.querySelector(".next");
 
-const quectionArray=[
+const SocrePage=document.querySelector('.container-socre-page');
+const playAgainbutton=document.querySelector('.play-again');
+
+const quizCount=document.querySelector('.Quize-count');
+const quizScoret=document.querySelector('.Quize-score');
+
+
+const quectionArray = [
     {
-        quection:"What is the capital in Sri lanaka",
+        quection: "What is the capital in Sri lanaka",
         Answer:
-        [
-            {AnswerText:'B1 colombo',iscorrect:false},
-            {AnswerText:'B2 horana' ,iscorrect:false},
-            {AnswerText:'B3 sri jayawardanapura',iscorrect:false},
-            {AnswerText:'B4 Kandy',iscorrect:false}
-        ]        
-            
+            [
+                { AnswerText: 'B1 colombo', iscorrect: false },
+                { AnswerText: 'B2 horana', iscorrect: false },
+                { AnswerText: 'B3 sri jayawardanapura', iscorrect: true },
+                { AnswerText: 'B4 Kandy', iscorrect: false }
+            ]
+
     },
     {
-        quection:"What is the long river in Sri lanaka",
+        quection: "What is the long river in Sri lanaka",
         Answer:
-        [
-            {AnswerText:'B1 Mahawali',iscorrect:false},
-            {AnswerText:'B2 kalu' ,iscorrect:false},
-            {AnswerText:'B3 walawe',iscorrect:false},
-            {AnswerText:'B4 nilwala',iscorrect:false}
-        ]        
-            
-    }
+            [
+                { AnswerText: 'B1 Mahawali', iscorrect: true },
+                { AnswerText: 'B2 kalu', iscorrect: false },
+                { AnswerText: 'B3 walawe', iscorrect: false },
+                { AnswerText: 'B4 nilwala', iscorrect: false }
+            ]
+
+    },
 ];
+
 
 let nextQuection;
 let Socre;
@@ -33,10 +42,10 @@ let Socre;
 
 //=====================Start Quiz=======================
 function startQuize() {
-    nextQuection=0;
-    Socre=0;
+    nextQuection = 0;
+    Socre = 0;
     ShowAnswers();
-    buttonNext.addEventListener("click",Move);
+    buttonNext.addEventListener("click", Move);
 
 }
 
@@ -46,27 +55,19 @@ function reset() {
     while (buttonElemnt.firstChild) {
         buttonElemnt.removeChild(buttonElemnt.firstChild);
     }
-    quectionElemt.innerText="";
+    quectionElemt.innerText = "";
 }
 
 
 //=====================Move to Next Quection=======================
 function Move() {
 
-    if (nextQuection<quectionArray.length) {
-    reset();
-    nextQuection++;
-    ShowAnswers();   
-    }else{
-        playAgain();
-        
+    if (nextQuection < quectionArray.length) {
+        reset();
+        nextQuection++;
+        ShowAnswers();
+        buttonNext.style.display="none";
     }
-}
-
-
-//=====================play Again=======================
-function playAgain() {
-    buttonNext.addEventListener("click",startQuize());
 }
 
 
@@ -74,27 +75,119 @@ function playAgain() {
 function ShowAnswers() {
 
     //================Show Quections=====================
-    if (nextQuection<quectionArray.length) {
-        
+    if (nextQuection < quectionArray.length) {
+
+        let hr=document.createElement("hr");
+        let quection = quectionArray[nextQuection];
+        quectionElemt.innerHTML = nextQuection + 1 + ". " + quection.quection;
+        quectionElemt.appendChild(hr);
+
+        quection.Answer.forEach((ans) => {
+            let button = document.createElement("button");
+            button.innerText = ans.AnswerText;
+            button.classList.add("btn")
+            button.dataset.correct = ans.iscorrect;
+            buttonElemnt.appendChild(button);
+
+            //==================Answers getting method(correction)========================
+            button.addEventListener("click", () => {
+                selectAnswers(button);
+            });
+
+
+
+        })
+    } else {
+        HomePage.style.display="none";
+        SocrePage.style.display="block";
+        quizCount.innerText=quectionArray.length;
+        quizScoret.innerText=Socre;
+
+
+        playAgainbutton.addEventListener("click",()=>{
+            HomePage.style.display="block";
+            SocrePage.style.display="none";
+            reset();
+            startQuize();
+        }); 
+
     
-    let quection=quectionArray[nextQuection]; 
-    quectionElemt.innerHTML=nextQuection+1+". "+quection.quection;
+    };
+
+}
+//=========================select quection======================
+function selectAnswers(button) {
     
 
-    quection.Answer.forEach((ans)=>{
-        let button=document.createElement("button");
-        button.innerText=ans.AnswerText;
-        button.classList.add("btn")
-        buttonElemnt.appendChild(button);
-        
-        buttonNext.innerText='next';
-})}else{
-    quectionElemt.innerHTML='DONE';
-    buttonNext.innerText='play Again';
-};
+    //=================my selection=========================
+    if (button.dataset.correct == "true") {
+       button.classList.add("correct");
+       Socre++;
+
+    } else {
+        button.classList.add("incorrect");
+    }
+    //=================correct selection=========================
+    Array.from(buttonElemnt.children).forEach((button) => {
+        if (button.dataset.correct == "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+   buttonNext.style.display="block";
 
 }
 
-
 //============================quize Start calling==================
-startQuize();
+const goToTheQuize=document.querySelector(".Go-to-the-Quize");
+const instructions=document.querySelector(".instructions");
+
+goToTheQuize.addEventListener('click',()=>{
+    instructions.style.display="none"; 
+    HomePage.style.display="block"; 
+    startQuize();
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//======================
+
+const submitbutton=document.querySelector('.submit-button');
+const textarea=document.querySelector('.textarea');
+const quitButton=document.querySelector('.quit-button');
+
+submitbutton.addEventListener('click',()=>{
+    console.log(textarea.value);
+    setLocalStorage(textarea.value);
+    textarea.value="";
+   getLocalStorage();
+    
+});
+quitButton.addEventListener('click',()=>{
+    SocrePage.style.display="none"; 
+    instructions.style.display="block"; 
+    
+});
+
+
+function setLocalStorage(value) {
+    localStorage.setItem('experince',value);
+}
+
+function getLocalStorage() {
+    console.log(localStorage.getItem('experince')); 
+}
